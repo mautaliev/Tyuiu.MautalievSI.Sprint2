ce4746e7654ad47be33ac4625094cc658c761acc
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-03-30 01:00:59 +0500

    Task4 dfgdfg

---
+++ Tyuiu.MautalievSI.Sprint2.Task4.V16.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MautalievSI.Sprint2.Task3.V16.Lib
+{
+    public class DataService : ISprint2Task4V16
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = (2 * x <= 2 * y)
+                ? Math.Pow(1 + 1 / (y * y), x)
+                : y - 1 / (x * x);
+
+            return Math.Round(z, 3);
+        }
+    }
+}
